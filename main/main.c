f6a78726bc0ef48ae58c4e1fe189db5de19e237b
AliceEnjoyer <Ilin06Serhii@gmail.com>
2026-03-05 18:07:12 +0200

    IMU added

---
+++ main/main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "driver/i2c_master.h"
+#include "sdkconfig.h"
+#include <bmi160.h>
+
+static const char *TAG = "GYM_TRACKER";
+
+#define MY_SCL 4
+#define MY_SDA 3
+#define MY_INT1_GPIO 1
+
+TaskHandle_t xTaskHandlePtr = NULL;
+
+typedef struct {
+    float accX, accY, accZ;
+    float gyroX, gyroY, gyroZ;
+} sensor_frame_t;
+
+static void IRAM_ATTR isr_new_data(void* arg)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    vTaskNotifyGiveFromISR(xTaskHandlePtr, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+void bmi160_task(void *pvParameters) 
+{
+    ESP_LOGI(TAG, "CONFIGURING BMI160");
+    bmi160_t bmi160_dev;
+    memset(&bmi160_dev.i2c_dev, 0, sizeof(i2c_dev_t));
+
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    io_conf.pin_bit_mask = (1ULL << MY_INT1_GPIO);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_down_en = 1;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+
+    // Установка службы прерываний и привязка функции-обработчика
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(MY_INT1_GPIO, isr_new_data, NULL);
+
+    // Инициализация датчика
+    ESP_ERROR_CHECK(bmi160_init(&bmi160_dev, 0x69, 0, MY_SDA, MY_SCL));
+    ESP_ERROR_CHECK(bmi160_self_test(&bmi160_dev));
+
+    bmi160_conf_t bmi160_conf = {
+        .accRange = BMI160_ACC_RANGE_2G,
+        .accOdr = BMI160_ACC_ODR_200HZ,
+        .accAvg = BMI160_ACC_LP_AVG_2,
+        .accMode = BMI160_PMU_ACC_NORMAL,
+        .gyrRange = BMI160_GYR_RANGE_125DPS,
+        .gyrOdr = BMI160_GYR_ODR_200HZ,
+        .gyrMode = BMI160_PMU_GYR_NORMAL,
+        .accUs = 0u
+    };
+
+    ESP_ERROR_CHECK(bmi160_start(&bmi160_dev, &bmi160_conf));
+    ESP_ERROR_CHECK(bmi160_calibrate(&bmi160_dev));
+
+    bmi160_int_out_conf_t intOutConf = {
+        .intPin = BMI160_PIN_INT1,
+        .intEnable = BMI160_INT_ENABLE,
+        .intOd = BMI160_INT_PUSH_PULL,
+        .intLevel = BMI160_INT_ACTIVE_HIGH // Поэтому на ESP32 мы используем POS_EDGE
+    };
+    ESP_ERROR_CHECK(bmi160_enable_int_new_data(&bmi160_dev, &intOutConf));
+
+    ESP_LOGI(TAG, "BMI160 Init Done. Waiting for data...");
+
+    bmi160_result_t result;
+
+    while(true) {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        esp_err_t ret = bmi160_read_data(&bmi160_dev, &result);
+        
+        if (ret == ESP_OK) {
+            ESP_LOGI(TAG, "ACC: X: %+.3f  Y: %+.3f  Z: %+.3f | GYRO: X: %+.3f  Y: %+.3f  Z: %+.3f", 
+                     result.accX, result.accY, result.accZ, 
+                     result.gyroX, result.gyroY, result.gyroZ);
+        } else {
+            ESP_LOGW(TAG, "Failed to read data!");
+        }
+    }
+
+    ESP_ERROR_CHECK(bmi160_free(&bmi160_dev));
+    vTaskDelete(NULL);
+}
+
+void app_main(void) 
+{
+    ESP_ERROR_CHECK(i2cdev_init());
+
+    xTaskCreate(bmi160_task, "bmi160_task", 4096, NULL, 5, &xTaskHandlePtr);
+}
---
+++ managed_components/esp-idf-lib__bmi160/README.md
@@ -0,0 +1,30 @@
+# esp-idf-lib/bmi160
+
+[![Build examples](https://github.com/esp-idf-lib/bmi160/actions/workflows//build.yml/badge.svg)](https://github.com/esp-idf-lib/bmi160/actions/workflows//build.yml)
+[![Build docs](https://github.com/esp-idf-lib/bmi160/actions/workflows//build-docs.yml/badge.svg)](https://github.com/esp-idf-lib/bmi160/actions/workflows//build-docs.yml)
+[![Validation](https://github.com/esp-idf-lib/bmi160/actions/workflows//validate-component.yml/badge.svg)](https://github.com/esp-idf-lib/bmi160/actions/workflows//validate-component.yml)
+
+ESP-IDF driver for ultra-small, low power, low noise 16 bit IMU BMI160
+
+* [Documentation](https://esp-idf-lib.github.io/bmi160/)
+* [Repository](https://github.com/esp-idf-lib/bmi160)
+* [Issues](https://github.com/esp-idf-lib/bmi160/issues)
+* [Discussions and questions](https://github.com/esp-idf-lib/core/discussions)
+* [Component page at the ESP Component Registry](https://components.espressif.com/components/esp-idf-lib/bmi160)
+
+## Installation
+
+```sh
+idf.py add-dependency esp-idf-lib/bmi160
+```
+
+## Support
+
+For questions and discussions about the component, please use
+[Discussions](https://github.com/esp-idf-lib/core/discussions)
+at [esp-idf-lib/core](https://github.com/esp-idf-lib/core).
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://github.com/esp-idf-lib/core/blob/main/CONTRIBUTING.md)
+at [esp-idf-lib/core](https://github.com/esp-idf-lib/core).
